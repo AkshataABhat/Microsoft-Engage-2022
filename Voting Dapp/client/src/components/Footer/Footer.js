8c884ed37c78b4350ded894e19407fe906754c75
Akshata Bhat <74967139+AkshataABhat@users.noreply.github.com>
2022-05-29 23:25:16 +0530

    Add files via upload

---
+++ Voting Dapp/client/src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          View this project on{" "}
+          <a
+            className="profile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <p>
+          Made with <i className="fas fa-heartbeat" /> by{" "}
+          <a
+            className="profile"
+    
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Akshata A Bhat
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
